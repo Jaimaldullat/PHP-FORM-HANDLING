f45eecd71f92fbaae374900ac367a67db3a731b4
Jaimal Dullat <jaimaldullat@gmail.com>
2019-03-07 18:25:19 -0500

    Changed Title

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (isset($_POST['send'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Feedback Form</title>
+    <title>Feedback Form | PHP</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
 </head>
